5adb018b28d89a52b484edcc4fcf9e156db2d42b
dytra <vancielnugraha@gmail.com>
2020-09-12 19:40:15 +0700

    finish basic feature

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const MainForm = () => {
   const [currentName, setCurrentName] = useState("");
   const [nameList, setNameList] = useState([]);
+  const [tempNameList, setTempNameList] = useState([]);
+  const [winner, setWinner] = useState();
 
-  const handleTambah = (e) => {
+  const handleSubmitNames = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
+    // console.log(name);
+    let newNameList = [];
+    newNameList = [...nameList, name];
+    setNameList(newNameList);
+    setCurrentName("");
+  }
+
+  const handleClickTambah = e => {
+    e.preventDefault();
+    const name = currentName;
     // console.log(name);
     let newNameList = [];
     newNameList = [...nameList, name];
@@ -20,34 +32,122 @@ const MainForm = () => {
 
   }
 
+  const handleSubmitRandom = e => {
+    e.preventDefault();
+    /* combine from temp */
+    let newNameList = nameList.map((item, index) => {
+      const found = tempNameList.find(itemTemp => {
+        return itemTemp.idx === index;
+      });
+      if (found) {
+        return found.name
+      } else {
+        return item;
+      }
+    })
+    console.log(newNameList);
+    /* get random */
+    const randomIndex = newNameList.length * Math.random() | 0;
+    const randomName = newNameList[randomIndex];
+    setWinner(randomName);
+    setNameList(newNameList);
+  }
+
+  const handleClickReset = () => {
+    setNameList([]);
+    setWinner(null);
+  }
+
+  const handleClickBack = () => {
+    setWinner(null);
+  }
+
+  const handleChangeNameByIndex = (index, e) => {
+    e.persist();
+    const val = e.target.value;
+    let curTempIndex;
+    const foundTemp = tempNameList.find((item, idx) => {
+      curTempIndex = idx;
+      return item.idx === index;
+    });
+    // console.log('foundtemp', foundTemp);
+    // console.log(tempNameList);
+    // return;
+    if (foundTemp) {
+      let newTempNameList = [...tempNameList];
+      newTempNameList[curTempIndex].name = val;
+      setTempNameList(newTempNameList)
+    } else {
+      const obj = {
+        idx: index,
+        name: val
+      }
+      // console.log(obj);
+      setTempNameList([...tempNameList, obj])
+    }
+
+  }
+
+  useEffect(() => {
+    // console.log(tempNameList);
+  }, [tempNameList]);
+
   return (
     <>
-      <ul>
-        {nameList.map((item, index) => {
-          return (
-            <li key={index}>{item}</li>
-          )
-        })}
-      </ul>
-      <form className="mb-5" onSubmit={handleTambah}>
-        <div className="field">
-          <div className="control">
+      {winner && (
+        <div className="mb-3">
+          <h3 >The winner is <strong>{winner}</strong></h3>
+          <button onClick={handleClickBack} className="button is-secondary is-small">Back</button>
+        </div>
+      )}
+      { !winner && nameList.map((item, index) => {
+        return (
+          <div className="columns mb-3" key={index + item}>
 
-            <div className="columns">
-              <div className="column is-4">
-                <input name="name" className="input" type="text" placeholder="Input" onChange={handleChangeName} autoComplete="off" value={currentName} required />
+            <div className="column is-4">
+              <div className="field" key={index}>
+                <div className="control is-flex">
+                  <span className="mr-3 is-flex" style={{ alignItems: 'center' }}>{index + 1}</span>
+                  <input key={'input' + index + item} name={`listed-name-${index}`} className="input" type="text" defaultValue={item} onInput={(e) => handleChangeNameByIndex(index, e)} />
+
+                </div>
               </div>
-              <div className="column is-3">
-                <a className="button is-secondary">Tambah</a>
-
-              </div>
-
+            </div>
+            <div className="column is-3">
+              <a className="button is-danger">Hapus</a>
             </div>
           </div>
-        </div>
-      </form>
+        )
+      })}
+
+      {
+        !winner && (
+          <>
+            <form className="mb-5" onSubmit={handleSubmitNames}>
+              <div className="field">
+                <div className="control">
+
+                  <div className="columns">
+
+                    <div className="column is-4">
+                      <input name="name" className="input" type="text" placeholder="Input" onChange={handleChangeName} autoComplete="off" value={currentName} required />
+                    </div>
+                    <div className="column is-3">
+                      <button className="button is-secondary" onClick={handleClickTambah}>Tambah</button>
+
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </form>
+
+          </>
+        )
+      }
       <div className="buttons">
-        <button className="button is-primary">Acakincuy</button>
+        <button className="button is-primary" onClick={handleSubmitRandom}>Acakincuy</button>
+        <button className="button is-secondary" onClick={handleClickReset}>Reset</button>
       </div>
     </>
   )
--- src/templates/DefaultTemplate.js
+++ src/templates/DefaultTemplate.js
@@ -3,13 +3,13 @@ import React from "react";
 const DefaultTemplate = ({ children }) => {
   return (
     <div className="container is-flex pt-3" style={{ flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', height: '100%' }}>
-      <header className="mb-3">
+      <header className="mb-5">
         <h1 className="title">Randominaja</h1>
         <p className="subtitle">Randomin apa aja yang kamu mau</p>
 
       </header>
 
-      <main className="mb-3" style={{width:'100%'}}>
+      <main className="mb-5" style={{ width: '100%' }}>
         {children}
       </main>
 
